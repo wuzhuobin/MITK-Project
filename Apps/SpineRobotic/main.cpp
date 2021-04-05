9849b7c5cf4f7f011317833d110689e22e0988ba
wuzhuobin <jiejin2022@163.com>
2021-04-06 01:35:14 +0800

    Create spine robotic

---
+++ Apps/SpineRobotic/main.cpp
@@ -0,0 +1,19 @@
+#include "MainWindow.h"
+// qt
+#include <QApplication>
+// mitk
+#include <QmitkRegisterClasses.h>
+#include <mitkRenderingManager.h>
+#include <mitkStandaloneDataStorage.h>
+
+int main(int argc, char *argv[])
+{
+  // Register Qmitk-dependent global instances
+  QmitkRegisterClasses();
+  mitk::RenderingManager::GetInstance()->SetDataStorage(mitk::StandaloneDataStorage::New());
+  QApplication app(argc, argv);
+  MainWindow mainWindow;
+  mainWindow.show();
+  mainWindow.Test();
+  return app.exec();
+}
