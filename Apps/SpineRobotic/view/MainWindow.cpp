765d420935d8fc41bff867b5530c3db14d6ee276
wuzhuobin <jiejin2022@163.com>
2021-05-07 14:59:41 -0700

    update menus

--- Apps/SpineRobotic/view/MainWindow.cpp
+++ Apps/SpineRobotic/view/MainWindow.cpp
@@ -2,11 +2,13 @@
 #include "IOController.h"
 #include "ui_MainWindow.h"
 
+// qt
+#include <QMenu>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) {
   this->ui->setupUi(this);
-
-
+  this->initializeMenu();
 
   QString fileName = QCoreApplication::arguments().size() > 1
                          ? QCoreApplication::arguments()[1]
@@ -24,4 +26,12 @@ void MainWindow::test() {}
 
 void MainWindow::initializeMenu()
 {
+    QMenu *menuShow = new QMenu(this);
+    this->ui->toolButtonShow->setMenu(menuShow);
+
+    QMenu *menuView = new QMenu(this);
+    this->ui->toolButtonView->setMenu(menuView);
+
+    QMenu *toolMenu = new QMenu(this);
+    this->ui->toolButtonTools->setMenu(toolMenu);
 }
