4b13fec050b377413f3bf4eac3bab07ee3c95f88
wuzhuobin <jiejin2022@163.com>
2021-06-13 05:26:24 -0700

    Add LoginDialog

---
+++ Apps/THA/view/LoginDialog.cpp
@@ -0,0 +1,88 @@
+﻿#include "LoginDialog.h"
+
+#include <qregularexpression.h>
+#include <qvalidator.h>
+
+#include "ui_LoginDialog.h"
+
+// #include "theme.h"
+// #include "session.h"
+// #include "account_manager/account_manager.h"
+
+const static struct LoginDaiglogResourceInit
+{
+    LoginDaiglogResourceInit() { Q_INIT_RESOURCE(resource); }
+} GInit;
+
+LoginDialog::LoginDialog(QWidget* parent) : QDialog(parent), mUi(new Ui::LoginDialog)
+{
+    mUi->setupUi(this);
+    auto* validator = new QRegularExpressionValidator(QRegularExpression("[a-zA-Z0-9]{25}"));
+    mUi->lineEditPassword->setValidator(validator);
+    // setWindowFlags(Qt::WindowStaysOnTopHint);
+    // initialize();
+}
+
+LoginDialog::~LoginDialog()
+{
+    delete mUi;
+}
+
+// void LoginDialog::initialize()
+// {
+//     // Theme::instance()->setFlowViewStyle(this);
+//     this->setWindowModality(Qt::WindowModality::NonModal);
+//     QPalette pa;
+//     pa.setColor(QPalette::WindowText, Qt::red);
+//     m_ui->loginInformation->setPalette(pa);
+//     // Theme::instance()->setTitleWidgetStyle(m_ui->widget);
+
+//     pRevalidotor = new QRegExpValidator(QRegExp("[a-zA-Z0-9]{25}"), this);
+//     m_ui->lineEditUserName->setPlaceholderText(tr("User ID"));
+//     m_ui->lineEditUserName->setValidator(pRevalidotor); //(only english and numbers)
+//     // Theme::instance()->setLineEditStyle(m_ui->lineEditUserName);
+//     m_ui->lineEditPassword->setPlaceholderText(tr("Password"));
+//     // Theme::instance()->setLineEditStyle(m_ui->lineEditPassword);
+//     m_ui->lineEditPassword->setValidator(pRevalidotor);
+//     m_ui->lineEditPassword->setEchoMode(QLineEdit::Password); //password input model（show****）
+//     connect(m_ui->loginBtn, &QPushButton::clicked, this, &LoginDialog::onLogin);
+// }
+
+// //Login Button Function
+// void LoginDialog::onLogin()
+// {
+//     QString name = m_ui->lineEditUserName->text();
+//     QString password = m_ui->lineEditPassword->text();
+
+//     if (true)
+//     // if (AccountManager::instance()->login(name, password))
+//     {
+//         this->close(); //should close this page first and then show create case page. or this page will exist.
+//         Session::instance()->setAccount(AccountManager::instance()->getAccount(name));
+//         emit signalLoginDialogResult(name, password);
+//     }
+//     else
+//     {
+//         showLoginWrongInfo();
+//     }
+// }
+
+// void LoginDialog::onUserVerify(bool nameVerifyResut, bool pswVerifyResut)
+// {
+//     m_verifyUserName = nameVerifyResut;
+//     m_verifyPassword = pswVerifyResut;
+// }
+
+// void LoginDialog::showLoginWrongInfo()
+// {
+//     if (!(m_verifyPassword && m_verifyUserName))
+//     {
+//         QString s = tr("Sorry. Account is wrong!");
+//         m_ui->loginInformation->setText(s);
+//     }
+// }
+
+void LoginDialog::on_pushButtonLogin_clicked(bool  /*checked*/)
+{
+    accept();
+}
---
+++ Apps/THA/view/LoginDialog.h
@@ -0,0 +1,36 @@
+﻿#ifndef LOGIN_DIALOG
+#define LOGIN_DIALOG
+
+// qt
+#include <qdialog.h>
+
+namespace Ui
+{
+class LoginDialog;
+}
+
+class LoginDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit LoginDialog(QWidget* parent = nullptr);
+    ~LoginDialog() override;
+
+    // signals:
+    //     void signalLoginDialogResult(const QString& userName, const QString& password);
+
+    // private slots:
+    //     void onLogin();
+
+private:
+    //     QRegExpValidator *pRevalidotor;
+    //     void initialize();
+    //     void onUserVerify(bool nameVerifyResut, bool pswVerifyResut);
+    //     void showLoginWrongInfo();
+    Ui::LoginDialog* mUi;
+    Q_SLOT void on_pushButtonLogin_clicked(bool checked = false);
+    //     bool m_verifyUserName = false;
+    //     bool m_verifyPassword = false;
+};
+#endif  // LOGIN_DIALOG
