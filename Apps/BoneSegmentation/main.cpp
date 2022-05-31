cb414d64fab9c296cb780494537c3b085fe0f7a2
wuzhuobin <jiejin2022@163.com>
2022-06-01 01:13:15 +0800

    Update otsu with mask slice by slice

--- Apps/BoneSegmentation/main.cpp
+++ Apps/BoneSegmentation/main.cpp
@@ -17,11 +17,15 @@
 #include <itkBinaryThresholdImageFilter.h>
 #include <itkConfidenceConnectedImageFilter.h>
 #include <itkConnectedComponentImageFilter.h>
+#include <itkExtractImageFilter.h>
 #include <itkMaskImageFilter.h>
 #include <itkOtsuMultipleThresholdsImageFilter.h>
 #include <itkOtsuThresholdImageFilter.h>
 #include <itkRelabelComponentImageFilter.h>
 #include <itkThresholdImageFilter.h>
+#include <itkTileImageFilter.h>
+
+using ImageType = itk::Image<short, 3>;
 
 int main2(int argc, char* argv[])
 {
@@ -79,32 +83,14 @@ int main2(int argc, char* argv[])
   return 0;
 }
 
-int main1(int argc, char* argv[])
+void otsuThreshold(ImageType* input, ImageType* output)
 {
-  auto imageFileName = "../../../Data/CTA 1.0 CE.nrrd";
-  auto image = mitk::IOUtil::Load<mitk::Image>(imageFileName);
-
-  using ImageType = itk::Image<short, 3>;
-  auto itkImage =
-      mitk::ImageToItkImage<ImageType::PixelType, ImageType::ImageDimension>(
-          image);
-
   using OtsuMultipleThresholdsFilterType =
       itk::OtsuMultipleThresholdsImageFilter<ImageType, ImageType>;
   auto otsuMultipleThresholdsFilter = OtsuMultipleThresholdsFilterType::New();
-  otsuMultipleThresholdsFilter->SetInput(itkImage);
+  otsuMultipleThresholdsFilter->SetInput(input);
   otsuMultipleThresholdsFilter->SetNumberOfThresholds(2);
   otsuMultipleThresholdsFilter->Update();
-
-  auto otsuMultipleThresholdsImage = mitk::Image::New();
-  otsuMultipleThresholdsImage->InitializeByItk(
-      otsuMultipleThresholdsFilter->GetOutput());
-  otsuMultipleThresholdsImage->SetVolume(
-      otsuMultipleThresholdsFilter->GetOutput()->GetBufferPointer());
-  otsuMultipleThresholdsImage->SetGeometry(image->GetGeometry());
-
-  mitk::IOUtil::Save(otsuMultipleThresholdsImage,
-                     "../../../Data/otsuMultipleThresholdsImage.nrrd");
 
   using BinaryThresholdFilterType =
       itk::BinaryThresholdImageFilter<ImageType, ImageType>;
@@ -116,55 +102,151 @@ int main1(int argc, char* argv[])
   binaryThresholdFilter->SetOutsideValue(0);
   binaryThresholdFilter->Update();
 
-  auto binaryThresholdedImage = mitk::Image::New();
-  binaryThresholdedImage->InitializeByItk(binaryThresholdFilter->GetOutput());
-  binaryThresholdedImage->SetVolume(
-      binaryThresholdFilter->GetOutput()->GetBufferPointer());
-  binaryThresholdedImage->SetGeometry(image->GetGeometry());
-  mitk::IOUtil::Save(binaryThresholdedImage,
-                     "../../../Data/binaryThresholded.nrrd");
-
-  //   using MaskImageFilter = itk::MaskImageFilter<ImageType, ImageType,
-  //   ImageType>; auto maskImageFilter = MaskImageFilter::New();
-  //   maskImageFilter->SetInput(itkImage);
-  //   maskImageFilter->SetMaskImage(binaryThresholdFilter->GetOutput());
-  //   maskImageFilter->SetOutsideValue(0);
-  //   maskImageFilter->Update();
-
-  //   auto maskedImage = mitk::Image::New();
-  //   maskedImage->InitializeByItk(maskImageFilter->GetOutput());
-  //   maskedImage->SetVolume(maskImageFilter->GetOutput()->GetBufferPointer());
-  //   maskedImage->SetGeometry(image->GetGeometry());
-  //   mitk::IOUtil::Save(maskedImage, "../../../Data/maskedImage.nrrd");
-
   using OtsuThresholdImageFilterType =
       itk::OtsuThresholdImageFilter<ImageType, ImageType, ImageType>;
   auto otsuThresholdImageFilter = OtsuThresholdImageFilterType::New();
-  otsuThresholdImageFilter->SetInput(itkImage);
+  otsuThresholdImageFilter->SetInput(input);
   otsuThresholdImageFilter->SetMaskImage(binaryThresholdFilter->GetOutput());
   otsuThresholdImageFilter->SetMaskValue(1);
   otsuThresholdImageFilter->MaskOutputOn();
   otsuThresholdImageFilter->SetInsideValue(0);
   otsuThresholdImageFilter->SetOutsideValue(1);
   otsuThresholdImageFilter->Update();
+  output->Graft(otsuThresholdImageFilter->GetOutput());
+}
 
-  //   otsuMultipleThresholdsFilter->SetInput(maskImageFilter->GetOutput());
-  //   otsuMultipleThresholdsFilter->SetNumberOfThresholds(2);
-  //   otsuMultipleThresholdsFilter->Update();
+void otsuThresholdSliceBySlice(ImageType* input, ImageType* output)
+{
+  using OtsuMultipleThresholdsFilterType =
+      itk::OtsuMultipleThresholdsImageFilter<ImageType, ImageType>;
+  auto otsuMultipleThresholdsFilter = OtsuMultipleThresholdsFilterType::New();
+  otsuMultipleThresholdsFilter->SetInput(input);
+  otsuMultipleThresholdsFilter->SetNumberOfThresholds(2);
+  otsuMultipleThresholdsFilter->Update();
 
-  //   otsuMultipleThresholdsImage->InitializeByItk(
-  //       otsuMultipleThresholdsFilter->GetOutput());
-  //   otsuMultipleThresholdsImage->SetVolume(
-  //       otsuMultipleThresholdsFilter->GetOutput()->GetBufferPointer());
-  //   otsuMultipleThresholdsImage->SetGeometry(image->GetGeometry());
+  using BinaryThresholdFilterType =
+      itk::BinaryThresholdImageFilter<ImageType, ImageType>;
+  auto binaryThresholdFilter = BinaryThresholdFilterType::New();
+  binaryThresholdFilter->SetInput(otsuMultipleThresholdsFilter->GetOutput());
+  binaryThresholdFilter->SetLowerThreshold(2);
+  binaryThresholdFilter->SetUpperThreshold(2);
+  binaryThresholdFilter->SetInsideValue(1);
+  binaryThresholdFilter->SetOutsideValue(0);
+  binaryThresholdFilter->Update();
 
-  //   mitk::IOUtil::Save(otsuMultipleThresholdsImage,
-  //                      "../../../Data/otsuMultipleThresholdsImage.nrrd");
+  using SliceType = itk::Image<short, 2>;
+  using TileImageFilterType = itk::TileImageFilter<SliceType, ImageType>;
+  auto tileImageFilter = TileImageFilterType::New();
+
+  auto region = input->GetBufferedRegion();
+  for (auto z = 0; z < region.GetSize()[2]; ++z)
+  {
+    MITK_INFO << "Progress: " << z;
+    auto index = region.GetIndex();
+    index[2] = z;
+    auto size = region.GetSize();
+    size[2] = 0;
+    ImageType::RegionType extractionRegion;
+    extractionRegion.SetIndex(index);
+    extractionRegion.SetSize(size);
+
+    using ExtractImageFilter = itk::ExtractImageFilter<ImageType, SliceType>;
+    auto extractImageFilterInput = ExtractImageFilter::New();
+    extractImageFilterInput->SetInput(input);
+    extractImageFilterInput->SetDirectionCollapseToSubmatrix();
+    extractImageFilterInput->SetExtractionRegion(extractionRegion);
+    extractImageFilterInput->Update();
+
+    auto extractImageFilterMask = ExtractImageFilter::New();
+    extractImageFilterMask->SetInput(binaryThresholdFilter->GetOutput());
+    extractImageFilterMask->SetDirectionCollapseToSubmatrix();
+    extractImageFilterMask->SetExtractionRegion(extractionRegion);
+    extractImageFilterMask->Update();
+
+    using OtsuThresholdImageFilterType =
+        itk::OtsuThresholdImageFilter<SliceType, SliceType, SliceType>;
+    auto otsuThresholdImageFilter = OtsuThresholdImageFilterType::New();
+    otsuThresholdImageFilter->SetInput(extractImageFilterInput->GetOutput());
+    otsuThresholdImageFilter->SetMaskImage(extractImageFilterMask->GetOutput());
+    otsuThresholdImageFilter->SetMaskValue(1);
+    otsuThresholdImageFilter->MaskOutputOn();
+    otsuThresholdImageFilter->SetInsideValue(0);
+    otsuThresholdImageFilter->SetOutsideValue(1);
+    otsuThresholdImageFilter->Update();
+
+    tileImageFilter->SetInput(z, otsuThresholdImageFilter->GetOutput());
+  }
+  TileImageFilterType::LayoutArrayType layout;
+  layout[0] = 1;
+  layout[1] = 1;
+  layout[2] = 0;
+  tileImageFilter->SetLayout(layout);
+  tileImageFilter->Update();
+  output->Graft(tileImageFilter->GetOutput());
+}
+
+void otsuMultipleThresholdSliceBySlice(ImageType* input, ImageType* output)
+{
+  using SliceType =
+      itk::Image<ImageType::PixelType, ImageType::ImageDimension - 1>;
+
+  using TileImageFilterType = itk::TileImageFilter<SliceType, ImageType>;
+  auto tileImageFilter = TileImageFilterType::New();
+
+  auto region = input->GetBufferedRegion();
+  for (auto z = 0; z < region.GetSize()[2]; ++z)
+  {
+    MITK_INFO << "Progress: " << z;
+    auto index = region.GetIndex();
+    index[2] = z;
+    auto size = region.GetSize();
+    size[2] = 0;
+    ImageType::RegionType extractionRegion;
+    extractionRegion.SetIndex(index);
+    extractionRegion.SetSize(size);
+
+    using ExtractImageFilter = itk::ExtractImageFilter<ImageType, SliceType>;
+    auto extractImageFilterInput = ExtractImageFilter::New();
+    extractImageFilterInput->SetInput(input);
+    extractImageFilterInput->SetDirectionCollapseToSubmatrix();
+    extractImageFilterInput->SetExtractionRegion(extractionRegion);
+    extractImageFilterInput->Update();
+
+    using OtsuMultipleThresholdsFilterType =
+        itk::OtsuMultipleThresholdsImageFilter<SliceType, SliceType>;
+    auto otsuMultipleThresholdsFilter = OtsuMultipleThresholdsFilterType::New();
+    otsuMultipleThresholdsFilter->SetInput(
+        extractImageFilterInput->GetOutput());
+    otsuMultipleThresholdsFilter->SetNumberOfThresholds(3);
+    otsuMultipleThresholdsFilter->Update();
+
+    tileImageFilter->SetInput(z, otsuMultipleThresholdsFilter->GetOutput());
+  }
+  TileImageFilterType::LayoutArrayType layout;
+  layout[0] = 1;
+  layout[1] = 1;
+  layout[2] = 0;
+  tileImageFilter->SetLayout(layout);
+  tileImageFilter->Update();
+  output->Graft(tileImageFilter->GetOutput());
+}
+
+int main(int argc, char* argv[])
+{
+  auto imageFileName = "../../../Data/CTA 1.0 CE.nrrd";
+  auto image = mitk::IOUtil::Load<mitk::Image>(imageFileName);
+
+  auto itkImage =
+      mitk::ImageToItkImage<ImageType::PixelType, ImageType::ImageDimension>(
+          image);
+
+  auto otsuThresholdImage = ImageType::New();
+  otsuThresholdSliceBySlice(itkImage, otsuThresholdImage);
+  MITK_INFO << otsuThresholdImage;
 
   auto otsuThresholdedImage = mitk::Image::New();
-  otsuThresholdedImage->InitializeByItk(otsuThresholdImageFilter->GetOutput());
-  otsuThresholdedImage->SetVolume(
-      otsuThresholdImageFilter->GetOutput()->GetBufferPointer());
+  otsuThresholdedImage->InitializeByItk(otsuThresholdImage.GetPointer());
+  otsuThresholdedImage->SetVolume(otsuThresholdImage->GetBufferPointer());
   otsuThresholdedImage->SetGeometry(image->GetGeometry());
 
   mitk::IOUtil::Save(otsuThresholdedImage,
@@ -173,7 +255,7 @@ int main1(int argc, char* argv[])
   using ConnectedComponentFilterType =
       itk::ConnectedComponentImageFilter<ImageType, ImageType>;
   auto connectedComponentFilter = ConnectedComponentFilterType::New();
-  connectedComponentFilter->SetInput(otsuThresholdImageFilter->GetOutput());
+  connectedComponentFilter->SetInput(otsuThresholdImage);
   connectedComponentFilter->Update();
 
   auto connectedComponentImage = mitk::Image::New();
@@ -208,7 +290,7 @@ int main1(int argc, char* argv[])
   return 0;
 }
 
-int main(int argc, char* argv[])
+int main3(int argc, char* argv[])
 {
   auto imageFileName = "../../../Data/thresholdedImage.nrrd";
   auto image = mitk::IOUtil::Load<mitk::Image>(imageFileName);
