a7950ee2559608956a6e1ac9b1abb61e0fde104b
wuzhuobin <jiejin2022@163.com>
2022-07-15 00:59:28 +0800

    Register Qmitk related class in main.cpp

--- Apps/Dental_Main/main.cpp
+++ Apps/Dental_Main/main.cpp
@@ -1,53 +1,35 @@
-﻿#include <QApplication>
-#include <QDebug>
-#include <QFile>
-#include <QTranslator>
-
-// me
+﻿/**
+ * @file main.cpp
+ * @author wuzhuobin (jiejin2022@163.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-07-15
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 #include "SCBDentalHub.h"
 #include "SCBDentalMainWindow.h"
-// #include "MACKeyconfirm.hpp"
-// #include "SCBDentalConfiguration.h"
 
-//#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
-#pragma comment(linker, "/SUBSYSTEM:console /ENTRY:mainCRTStartup")
+// qt
+#include <QApplication>
+
+// mitk
+#include <QmitkRegisterClasses.h>
+#include <mitkRenderingManager.h>
+#include <mitkStandaloneDataStorage.h>
 
 int main(int argc, char* argv[])
 {
-  // using namespace QsLogging;
-
+  QmitkRegisterClasses();
+  mitk::RenderingManager::GetInstance()->SetDataStorage(
+      mitk::StandaloneDataStorage::New());
   QApplication app(argc, argv);
-  //   SCBSceneConfiguration();
 
-  QTranslator qtTranslator;
-  qtTranslator.load("lang_" + QLocale::system().name());
-  // qtTranslator.load("lang_zh_CN.ts");
-  app.installTranslator(&qtTranslator);
-
-  // QFile styleFile(":/HyperBlue/hyperblue/styles.qss");
-  // if (styleFile.open(QIODevice::ReadOnly)) {
-  //	app.setStyleSheet(styleFile.readAll());
-  // }
   SCBDentalHub hub;
-  if (hub.mainWindow->slotMacChecking())
-  {
-    hub.mainWindow->show();
-    // hub.mainWindow->showFullScreen();
-    hub.mainWindow->on_tabWidgetCentral_currentChanged(0);
-    hub.mainWindow->slotWelcomeWizard();
-    // hub.slotInitializationImages(QStringList() <<
-    // QCoreApplication::applicationDirPath() +
-    //	"/HardCode/seg_resample.nii.gz");
-  }
-
-  ////QPixmap pixmap(":/HyperBlue/hyperblue/icon/product-splashScreen.png");
-  // QPixmap pixmap(":/HyperBlue/hyperblue/icon/Orthopedics-splashscreen2.png");
-  // QSplashScreen splash(pixmap);
-  // splash.show();
-  // app.processEvents();
-
-  // splash.finish(NULL);
-  // hub.slotImport("C:/Users/jieji/Desktop/mandible.xml", true);
+  hub.mainWindow->show();
+  hub.mainWindow->on_tabWidgetCentral_currentChanged(0);
+  hub.mainWindow->slotWelcomeWizard();
 
   return app.exec();
 }
--- Apps/THA_Main/main.cpp
+++ Apps/THA_Main/main.cpp
@@ -1,3 +1,13 @@
+/**
+ * @file main.cpp
+ * @author wuzhuobin (jiejin2022@163.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-07-15
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 #include "MainWindow.h"
 
 // qt
@@ -8,11 +18,12 @@
 #include <mitkRenderingManager.h>
 #include <mitkStandaloneDataStorage.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
   // Register Qmitk-dependent global instances
   QmitkRegisterClasses();
-  mitk::RenderingManager::GetInstance()->SetDataStorage(mitk::StandaloneDataStorage::New());
+  mitk::RenderingManager::GetInstance()->SetDataStorage(
+      mitk::StandaloneDataStorage::New());
   QApplication app(argc, argv);
   MainWindow w;
   w.show();
